46ecc8a394bfe40e35c2bb44e8acbf9f543f63c6
Chandan-Kumar-111 <134257592+Chandan-Kumar-111@users.noreply.github.com>
2023-05-22 11:34:22 +0530

    Add files via upload

---
+++ firstps.ps1
@@ -0,0 +1,23 @@
+﻿#Create WebApp
+#variables
+$appname="mysiteapp$(Get-Random)"
+$res="myres"
+$loc= "Southeast Asia"
+$plan="appplan"
+
+#create Resource Group
+
+New-AzResourceGroup -Name $res -Location $loc
+
+#create Resource plan
+
+New-AzAppServicePlan -Name $plan -Location $loc -Tier Basic -ResourceGroupName $res
+
+#create web app
+
+New-AzWebApp -Name $appname -Location $loc -ResourceGroupName $res -AppServicePlan $plan 
+
+#git configuration
+
+Set-AzResource -Properties gitproperty -ResourceGroupName $res -ResourceType Microsoft.Web/sites/sourcecontrols -ResourceName $appname/web -ApiVersion 2015-08-01 -Force
+
