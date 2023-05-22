a1a2a6f054d95e184f7e0a9a2ca87fccb7a2c725
Chandan-Kumar-111 <134257592+Chandan-Kumar-111@users.noreply.github.com>
2023-05-22 11:57:21 +0530

    Add files via upload
    Create DB in Azure Using Powershell

---
+++ database.ps1
@@ -0,0 +1,24 @@
+﻿#create DB
+#variables
+$res="myres"
+$loc= "Southeast Asia"
+$dbserver="test$(Get-Random)"
+$dbname="sampledb"
+$dblogin="dbuser"
+$dbpwd="user12345678"
+$sqlcred= New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $dblogin , (ConvertTo-SecureString -String $dbpwd -AsPlainText -Force)
+$startIp=""
+$endIp=""
+
+
+#create DB server
+
+New-AzSqlServer -Servername $dbserver -Location $loc -ResourceGroupName $res -SqlAdministratorCredentials $sqlcred
+
+#create DB
+
+New-AzSqlDatabase -DatabaseName $dbname -ServerName $dbserver -ResourceGroupName $res
+
+#Add Firewall
+
+New-AzSqlServerFirewallRule -FirewallRuleName "clientip1" -StartIpAddress $startIp -EndIpAddress $endIp -ServerName $dbserver -ResourceGroupName $res
